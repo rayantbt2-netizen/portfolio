981e3b2734188c9930078b3185ab199747f739f2
rayantbt2-netizen <rayantbt2@gmail.com>
2026-05-01 13:30:17 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
-
-  /* TYPEWRITER */
-  const title = document.querySelector(".title");
   const text = "Bienvenue sur mon portfolio";
+  const title = document.querySelector(".title");
 
   let i = 0;
-  title.innerHTML = "";
 
   function write() {
     if (i < text.length) {
@@ -16,19 +13,4 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 
   write();
-
-  /* SCROLL ANIMATION */
-  const elements = document.querySelectorAll(".card");
-
-  const obs = new IntersectionObserver(entries => {
-    entries.forEach(e => {
-      if (e.isIntersecting) e.target.classList.add("show");
-    });
-  });
-
-  elements.forEach(el => {
-    el.classList.add("hidden");
-    obs.observe(el);
-  });
-
 });
