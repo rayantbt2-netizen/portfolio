d9ac3b2aa52eddeb88d2d4098e2e88360c1c4c3d
rayantbt2-netizen <rayantbt2@gmail.com>
2026-05-01 13:40:32 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const text = "Bienvenue sur mon portfolio";
+
+  /* TEXT ANIMATION */
   const title = document.querySelector(".title");
+  const text = "Bienvenue sur mon portfolio";
 
   let i = 0;
 
@@ -8,9 +10,26 @@ document.addEventListener("DOMContentLoaded", () => {
     if (i < text.length) {
       title.innerHTML += text[i];
       i++;
-      setTimeout(write, 70);
+      setTimeout(write, 60);
     }
   }
 
   write();
+
+  /* SCROLL ANIMATION */
+  const elements = document.querySelectorAll(".timeline-item, .card");
+
+  const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("show");
+      }
+    });
+  });
+
+  elements.forEach(el => {
+    el.classList.add("hidden");
+    observer.observe(el);
+  });
+
 });
